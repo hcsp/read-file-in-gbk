26eaf453bd6708c88fd583cfdf3a5ea460c74ef1
SuyuZhuang <zhuangsuyu1993@163.com>
2019-10-01 15:42:07 +0800

    修改为readAllBytes

--- src/main/java/com/github/hcsp/string/GbkFileReader.java
+++ src/main/java/com/github/hcsp/string/GbkFileReader.java
@@ -2,9 +2,7 @@ package com.github.hcsp.string;
 
 import java.io.File;
 import java.io.IOException;
-import java.nio.charset.Charset;
 import java.nio.file.Files;
-import java.util.List;
 
 public class GbkFileReader {
     public static void main(String[] args) {
@@ -13,16 +11,13 @@ public class GbkFileReader {
     }
 
     public String readFileWithGBK(File file) {
-        StringBuilder stringBuilder = new StringBuilder();
+        byte[] bytes;
         try {
-            List<String> stringList = Files.readAllLines(file.toPath(), Charset.forName("GBK"));
-            for (String str : stringList) {
-                stringBuilder.append(str).append("\n");
-            }
-            stringBuilder.delete(stringBuilder.length() - 1, stringBuilder.length());
+            bytes = Files.readAllBytes(file.toPath());
+            return new String(bytes, "GBK");
         } catch (IOException e) {
             e.printStackTrace();
         }
-        return stringBuilder.toString();
+        return null;
     }
 }
