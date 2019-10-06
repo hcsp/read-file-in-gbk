ddca7b93d1e05c94a92f233e2ed932762d50bd52
JayChenFE <jaychenfe@foxmail.com>
2019-10-06 15:00:24 +0800

    修改问题

--- src/main/java/com/github/hcsp/string/GbkFileReader.java
+++ src/main/java/com/github/hcsp/string/GbkFileReader.java
@@ -4,7 +4,6 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.charset.Charset;
 import java.nio.file.Files;
-import java.util.stream.Collectors;
 
 public class GbkFileReader {
     public static void main(String[] args) throws IOException {
@@ -13,6 +12,6 @@ public class GbkFileReader {
     }
 
     public String readFileWithGBK(File file) throws IOException {
-        return Files.readAllLines(file.toPath(), Charset.forName("GBK")).stream().collect(Collectors.joining());
+        return new String(Files.readAllBytes(file.toPath()), Charset.forName("GBK"));
     }
 }
