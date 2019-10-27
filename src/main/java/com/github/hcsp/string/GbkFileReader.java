02e6ef13e53d513134c24b44d934e4d48e0c16c3
JinLingfeng <37367546+JinLingfeng@users.noreply.github.com>
2019-10-27 22:00:48 +0800

    GBK (#12)
    GBK (#12)

--- src/main/java/com/github/hcsp/string/GbkFileReader.java
+++ src/main/java/com/github/hcsp/string/GbkFileReader.java
@@ -1,14 +1,21 @@
 package com.github.hcsp.string;
 
-import java.io.File;
+import java.io.*;
+import java.nio.charset.Charset;
 
 public class GbkFileReader {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         File projectDir = new File(System.getProperty("basedir", System.getProperty("user.dir")));
         System.out.println(new GbkFileReader().readFileWithGBK(new File(projectDir, "gbk.txt")));
     }
 
-    public String readFileWithGBK(File file) {
-        return null;
+    public String readFileWithGBK(File file) throws IOException {
+        BufferedReader bfr = new BufferedReader(new InputStreamReader(new FileInputStream(file), Charset.forName("GBK")));
+        int c;
+        StringBuilder sb = new StringBuilder();
+        while ((c = bfr.read()) != -1) {
+            sb.append((char) c);
+        }
+        return sb.toString();
     }
 }
