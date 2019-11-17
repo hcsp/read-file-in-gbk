aae109969260ef69520028f94084901cf5b92584
QinLi Tang <49433104+uuflyer@users.noreply.github.com>
2019-11-17 21:59:16 +0800

    read-file-in-gbk (#15)
    read-file-in-gbk (#15)

--- src/main/java/com/github/hcsp/string/GbkFileReader.java
+++ src/main/java/com/github/hcsp/string/GbkFileReader.java
@@ -1,6 +1,10 @@
 package com.github.hcsp.string;
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.util.stream.Collectors;
 
 public class GbkFileReader {
     public static void main(String[] args) {
@@ -9,6 +13,11 @@ public class GbkFileReader {
     }
 
     public String readFileWithGBK(File file) {
+        try {
+            return Files.readAllLines(file.toPath(), Charset.forName("GBK")).stream().collect(Collectors.joining("\n"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         return null;
     }
 }
