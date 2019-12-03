445a86ae1bbf4457da6b6746a052c9cae87e8472
magicP <42081285+magicP@users.noreply.github.com>
2019-12-03 20:23:10 +0800

    读取一个GBK编码的文件 (#19)
    读取一个GBK编码的文件 (#19)

--- src/main/java/com/github/hcsp/string/GbkFileReader.java
+++ src/main/java/com/github/hcsp/string/GbkFileReader.java
@@ -1,6 +1,8 @@
 package com.github.hcsp.string;
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
 
 public class GbkFileReader {
     public static void main(String[] args) {
@@ -9,6 +11,10 @@ public class GbkFileReader {
     }
 
     public String readFileWithGBK(File file) {
-        return null;
+        try {
+            return new String(Files.readAllBytes(file.toPath()), "GBK");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
