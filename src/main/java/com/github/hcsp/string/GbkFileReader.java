4ef08769f289c32f71e1d7e0c707953dbefb24bc
emmmm <3108775859@qq.com>
2020-01-08 19:47:12 +0800

    GBK编码的文件

--- src/main/java/com/github/hcsp/string/GbkFileReader.java
+++ src/main/java/com/github/hcsp/string/GbkFileReader.java
@@ -1,14 +1,21 @@
 package com.github.hcsp.string;
 
-import java.io.File;
+import java.io.*;
 
 public class GbkFileReader {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         File projectDir = new File(System.getProperty("basedir", System.getProperty("user.dir")));
         System.out.println(new GbkFileReader().readFileWithGBK(new File(projectDir, "gbk.txt")));
     }
 
-    public String readFileWithGBK(File file) {
-        return null;
+    public String readFileWithGBK(File file) throws IOException {
+//        byte[] bytes = Files.readAllBytes(file.toPath());
+//        String str = new String(bytes, "GBK");
+//        System.out.println();
+//        return null;
+        BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(file), "GBK"));
+//        System.out.println(br.lines().collect(Collectors.joining("\n")));
+//        System.out.println(br.readLine());
+        return br.readLine();
     }
 }
