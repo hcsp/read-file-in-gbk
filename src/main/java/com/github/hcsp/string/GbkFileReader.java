4e533834b4078331db7f16a5a8817818895ccd90
NervousOrange <zch693922442@outlook.com>
2020-01-26 20:49:51 +0100

    from NervousOrange: Assignment 2 of String.

--- src/main/java/com/github/hcsp/string/GbkFileReader.java
+++ src/main/java/com/github/hcsp/string/GbkFileReader.java
@@ -1,6 +1,10 @@
 package com.github.hcsp.string;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
 
 public class GbkFileReader {
     public static void main(String[] args) {
@@ -9,6 +13,23 @@ public class GbkFileReader {
     }
 
     public String readFileWithGBK(File file) {
-        return null;
+        Charset charset = Charset.forName("GBK");
+        BufferedReader bufferedReader;
+        try {
+            bufferedReader = Files.newBufferedReader(file.toPath(), charset);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        StringBuilder stringInFile = new StringBuilder();
+        String temp;
+        try {
+             while ((temp = bufferedReader.readLine()) != null) {
+                 stringInFile.append(temp).append("\n");
+             }
+             stringInFile.deleteCharAt(stringInFile.length() - 1);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return stringInFile.toString();
     }
 }
