f14657eddac357df297a7778dcd25a87ac39edab
zaker-666 <1075151259china@gmail.com>
2020-01-27 15:50:57 +0800

    finish and push

--- src/main/java/com/github/hcsp/string/GbkFileReader.java
+++ src/main/java/com/github/hcsp/string/GbkFileReader.java
@@ -1,6 +1,10 @@
 package com.github.hcsp.string;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.nio.ByteBuffer;
+import java.nio.charset.Charset;
+import java.nio.charset.CharsetDecoder;
 
 public class GbkFileReader {
     public static void main(String[] args) {
@@ -9,6 +13,18 @@ public class GbkFileReader {
     }
 
     public String readFileWithGBK(File file) {
+        StringBuilder s = new StringBuilder();
+        Charset gbk = Charset.forName("GBK");
+        CharsetDecoder charsetDecoder = gbk.newDecoder();
+        byte[] bytes = new byte[11];
+        try (FileInputStream fileInputStream = new FileInputStream(file)) {
+            while (fileInputStream.read(bytes) != -1) {
+                s.append(charsetDecoder.decode(ByteBuffer.wrap(bytes)));
+            }
+            return s.toString().trim();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
         return null;
     }
 }
