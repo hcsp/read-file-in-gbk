10b905bbce61070b9e241783cbf1402b728dc009
Xi Pu <30915281+XiPu99@users.noreply.github.com>
2020-01-28 04:28:37 -0500

    read file with GBK encoding (#29)
    read file with GBK encoding (#29)

--- src/main/java/com/github/hcsp/string/GbkFileReader.java
+++ src/main/java/com/github/hcsp/string/GbkFileReader.java
@@ -1,6 +1,6 @@
 package com.github.hcsp.string;
 
-import java.io.File;
+import java.io.*;
 
 public class GbkFileReader {
     public static void main(String[] args) {
@@ -9,6 +9,21 @@ public class GbkFileReader {
     }
 
     public String readFileWithGBK(File file) {
-        return null;
+
+        try {
+            BufferedReader in = new BufferedReader(
+                    new InputStreamReader(new FileInputStream(file), "GBK"));
+            StringBuilder result = new StringBuilder();
+            String line;
+            while ((line = in.readLine()) != null) {
+                result.append(line).append('\n');
+            }
+
+            return result.substring(0, result.length() - 1); // remove extra new line character
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return "";
     }
 }
