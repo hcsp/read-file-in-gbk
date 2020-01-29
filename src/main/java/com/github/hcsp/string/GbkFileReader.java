1178b68e1bce7cdd93c4b5206873db898fad4ffa
monalisali <34482115+monalisali@users.noreply.github.com>
2020-01-29 16:26:05 +0800

    done (#30)
    done (#30)

--- src/main/java/com/github/hcsp/string/GbkFileReader.java
+++ src/main/java/com/github/hcsp/string/GbkFileReader.java
@@ -1,6 +1,10 @@
 package com.github.hcsp.string;
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 
 public class GbkFileReader {
     public static void main(String[] args) {
@@ -9,6 +13,11 @@ public class GbkFileReader {
     }
 
     public String readFileWithGBK(File file) {
-        return null;
+        try {
+            byte[] bytes = Files.readAllBytes(Paths.get(file.getPath()));
+            return new String(bytes, "GBK");
+        } catch (IOException e) {
+            throw new RuntimeException();
+        }
     }
 }
