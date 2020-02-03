c78c898239d188fbf7f6a82c7a713be126239075
xdcgh <44773625+xdcgh@users.noreply.github.com>
2020-02-03 14:43:31 +0800

    完成读取一个GBK编码的文件 (#33)
    完成读取一个GBK编码的文件 (#33)

--- src/main/java/com/github/hcsp/string/GbkFileReader.java
+++ src/main/java/com/github/hcsp/string/GbkFileReader.java
@@ -1,6 +1,9 @@
 package com.github.hcsp.string;
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 public class GbkFileReader {
     public static void main(String[] args) {
@@ -9,6 +12,12 @@ public class GbkFileReader {
     }
 
     public String readFileWithGBK(File file) {
-        return null;
+        try {
+            byte[] bytes = Files.readAllBytes(Paths.get(file.getPath()));
+            return new String(bytes, "GBK");
+        } catch (IOException e) {
+            throw new RuntimeException("读取文件失败");
+        }
+
     }
 }
