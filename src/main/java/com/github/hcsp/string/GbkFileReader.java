1dddb95bfaac72036bfda64e9dead9240d8861d3
ws1603 <18388866683@163.com>
2020-02-18 17:53:58 +0800

    读取GBK编码的文件

--- src/main/java/com/github/hcsp/string/GbkFileReader.java
+++ src/main/java/com/github/hcsp/string/GbkFileReader.java
@@ -1,14 +1,24 @@
 package com.github.hcsp.string;
 
-import java.io.File;
+import java.io.*;
+import java.nio.charset.Charset;
 
 public class GbkFileReader {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         File projectDir = new File(System.getProperty("basedir", System.getProperty("user.dir")));
         System.out.println(new GbkFileReader().readFileWithGBK(new File(projectDir, "gbk.txt")));
     }
 
-    public String readFileWithGBK(File file) {
-        return null;
+    public String readFileWithGBK(File file) throws IOException {
+        int code;
+        byte[] b = new byte[1024];
+        int count = 0;
+        InputStream is = new FileInputStream(file);
+        code = is.read();
+        while (code != -1) {
+            b[count++] = (byte) code;
+            code = is.read();
+        }
+        return new String(b, 0, count, Charset.forName("GBK"));
     }
 }
