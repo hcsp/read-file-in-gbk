0e11d2a076c1dcc6bfc759332b74c3d3d526abb7
ws1603 <48832455+ws1603@users.noreply.github.com>
2020-02-21 16:53:49 +0800

    read-file-in-gbk (#40)
    read-file-in-gbk (#40)

--- src/main/java/com/github/hcsp/string/GbkFileReader.java
+++ src/main/java/com/github/hcsp/string/GbkFileReader.java
@@ -1,14 +1,26 @@
 package com.github.hcsp.string;
 
-import java.io.File;
+import java.io.*;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.util.List;
 
 public class GbkFileReader {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         File projectDir = new File(System.getProperty("basedir", System.getProperty("user.dir")));
         System.out.println(new GbkFileReader().readFileWithGBK(new File(projectDir, "gbk.txt")));
     }
 
-    public String readFileWithGBK(File file) {
-        return null;
+    public String readFileWithGBK(File file) throws IOException {
+        List<String> strings = Files.readAllLines(file.toPath(), Charset.forName("GBK"));
+        StringBuilder sb = new StringBuilder();
+        if (!strings.isEmpty()) {
+            sb.append(strings.remove(0));
+            while (strings.iterator().hasNext()) {
+                sb.append("\n");
+                sb.append(strings.remove(0));
+            }
+        }
+        return sb.toString();
     }
 }
