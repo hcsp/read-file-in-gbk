f431a542b4ba6216a9cb48a6656cb816aacfda52
Maricaya <915270549@qq.com>
2020-02-26 23:22:53 +0800

    feat: 读取GBK编码的文件

--- src/main/java/com/github/hcsp/string/GbkFileReader.java
+++ src/main/java/com/github/hcsp/string/GbkFileReader.java
@@ -1,14 +1,20 @@
 package com.github.hcsp.string;
 
+
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 public class GbkFileReader {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         File projectDir = new File(System.getProperty("basedir", System.getProperty("user.dir")));
         System.out.println(new GbkFileReader().readFileWithGBK(new File(projectDir, "gbk.txt")));
     }
 
-    public String readFileWithGBK(File file) {
-        return null;
+    public String readFileWithGBK(File file) throws IOException {
+        byte[] bytes = Files.readAllBytes(Paths.get(file.getPath()));
+
+        return new String(bytes, "gbk");
     }
 }
