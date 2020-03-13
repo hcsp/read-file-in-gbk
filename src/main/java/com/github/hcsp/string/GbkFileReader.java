5e8d25f041bf4af8a306617935c8bcaabdfb2868
KisetsuLee <42855253+KisetsuLee@users.noreply.github.com>
2020-03-13 17:38:08 +0800

    读取gbk文件 (#49)
    读取gbk文件 (#49)

--- src/main/java/com/github/hcsp/string/GbkFileReader.java
+++ src/main/java/com/github/hcsp/string/GbkFileReader.java
@@ -1,6 +1,6 @@
 package com.github.hcsp.string;
 
-import java.io.File;
+import java.io.*;
 
 public class GbkFileReader {
     public static void main(String[] args) {
@@ -9,6 +9,20 @@ public class GbkFileReader {
     }
 
     public String readFileWithGBK(File file) {
-        return null;
+        try {
+            InputStream is = new FileInputStream(file);
+            InputStreamReader ir = new InputStreamReader(is, "gbk");
+            BufferedReader br = new BufferedReader(ir);
+
+            StringBuilder str = new StringBuilder();
+            char[] buf = new char[10];
+            int hasChar = 0;
+            while ((hasChar = br.read(buf, 0, buf.length)) != -1) {
+                str.append(buf, 0, hasChar);
+            }
+            return str.toString();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
