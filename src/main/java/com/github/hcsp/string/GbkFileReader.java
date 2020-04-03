ea21a5efdf9b2d2f705c884a1a5dbc153410ba6b
Airtoit <51944383+Airtoit@users.noreply.github.com>
2020-04-04 00:02:24 +0800

    读取一个GBK编码的文件 (#54)
    读取一个GBK编码的文件 (#54)

--- src/main/java/com/github/hcsp/string/GbkFileReader.java
+++ src/main/java/com/github/hcsp/string/GbkFileReader.java
@@ -1,6 +1,9 @@
 package com.github.hcsp.string;
 
-import java.io.File;
+import java.io.*;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.util.stream.Collectors;
 
 public class GbkFileReader {
     public static void main(String[] args) {
@@ -9,6 +12,10 @@ public class GbkFileReader {
     }
 
     public String readFileWithGBK(File file) {
-        return null;
+        try {
+            return Files.readAllLines(file.toPath(), Charset.forName("GBK")).stream().collect(Collectors.joining("\n"));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
