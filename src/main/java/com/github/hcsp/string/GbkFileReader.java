ce553645b9a590e7a587b67ac9e0b5dd1c8a589c
gofocus <go_focus@live.cn>
2020-05-09 11:12:37 +0800

    使用Stream读取文件 (#66)
    使用Stream读取文件 (#66)

--- src/main/java/com/github/hcsp/string/GbkFileReader.java
+++ src/main/java/com/github/hcsp/string/GbkFileReader.java
@@ -1,14 +1,20 @@
 package com.github.hcsp.string;
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
 
 public class GbkFileReader {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         File projectDir = new File(System.getProperty("basedir", System.getProperty("user.dir")));
         System.out.println(new GbkFileReader().readFileWithGBK(new File(projectDir, "gbk.txt")));
     }
 
-    public String readFileWithGBK(File file) {
-        return null;
+    public String readFileWithGBK(File file) throws IOException {
+        StringBuilder sb = new StringBuilder();
+        Files.lines(file.toPath(), Charset.forName("GBK")).map(s -> s + '\n').forEachOrdered(sb::append);
+        sb.deleteCharAt(sb.length() - 1);
+        return sb.toString();
     }
 }
