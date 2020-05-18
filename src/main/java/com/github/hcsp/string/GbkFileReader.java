7e9a5fbfa7f44746b6559927968694503c773a03
fuduo <329794799@qq.com>
2020-05-19 05:18:51 +0800

    GbkFileReader

--- src/main/java/com/github/hcsp/string/GbkFileReader.java
+++ src/main/java/com/github/hcsp/string/GbkFileReader.java
@@ -1,6 +1,10 @@
 package com.github.hcsp.string;
 
-import java.io.*;
+import java.io.File;
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.FileInputStream;
 
 public class GbkFileReader {
     public static void main(String[] args) throws IOException {
