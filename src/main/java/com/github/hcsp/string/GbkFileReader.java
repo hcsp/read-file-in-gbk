1ee46412af9559230e2deb6544da624d61e2b1b8
Song <36783363+JoeyPine@users.noreply.github.com>
2020-05-18 20:47:30 +0800

    Joey pine branch (#69)
    Joey pine branch (#69)

--- src/main/java/com/github/hcsp/string/GbkFileReader.java
+++ src/main/java/com/github/hcsp/string/GbkFileReader.java
@@ -1,14 +1,21 @@
 package com.github.hcsp.string;
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 public class GbkFileReader {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         File projectDir = new File(System.getProperty("basedir", System.getProperty("user.dir")));
         System.out.println(new GbkFileReader().readFileWithGBK(new File(projectDir, "gbk.txt")));
     }
 
-    public String readFileWithGBK(File file) {
-        return null;
+    public String readFileWithGBK(File file) throws IOException {
+        try {
+            return new String(Files.readAllBytes(Paths.get(file.getPath())), "GBK");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
