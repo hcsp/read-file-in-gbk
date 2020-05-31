70c9f3dcba73c6ab6eb952f4a967a219ec25c99e
zhangzh <zhangzh@cnlemon.net>
2020-05-31 22:33:14 +0800

    完成挑战

--- src/main/java/com/github/hcsp/string/GbkFileReader.java
+++ src/main/java/com/github/hcsp/string/GbkFileReader.java
@@ -1,6 +1,12 @@
 package com.github.hcsp.string;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
 
 public class GbkFileReader {
     public static void main(String[] args) {
@@ -9,6 +15,17 @@ public class GbkFileReader {
     }
 
     public String readFileWithGBK(File file) {
+        Charset gbkCharset = Charset.forName("GBK");
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(file), gbkCharset))) {
+            StringBuilder stringBuilder = new StringBuilder();
+            String temp;
+            while ((temp = reader.readLine()) != null) {
+                stringBuilder.append(new String(temp.getBytes(), StandardCharsets.UTF_8));
+            }
+            return stringBuilder.toString();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         return null;
     }
 }
