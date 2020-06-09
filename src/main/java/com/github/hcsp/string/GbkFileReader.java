6f452bf9116c47b5df5f2381ff47af4903531481
kongmu373 <kongmu1235@gmail.com>
2020-06-09 10:59:27 +0800

    完成读取一个GBK编码的文件

--- src/main/java/com/github/hcsp/string/GbkFileReader.java
+++ src/main/java/com/github/hcsp/string/GbkFileReader.java
@@ -1,6 +1,11 @@
 package com.github.hcsp.string;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.Arrays;
 
 public class GbkFileReader {
     public static void main(String[] args) {
@@ -9,6 +14,19 @@ public class GbkFileReader {
     }
 
     public String readFileWithGBK(File file) {
+
+        try (InputStream in = new FileInputStream(file);
+             InputStreamReader inputStreamReader = new InputStreamReader(in, "GBK")) {
+            final char[] bytes = new char[1024];
+            int len;
+            StringBuilder sb = new StringBuilder();
+            while ((len = inputStreamReader.read(bytes)) != -1) {
+                sb.append(Arrays.copyOf(bytes, len));
+            }
+            return sb.toString();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         return null;
     }
 }
