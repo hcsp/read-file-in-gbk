b80f054acb63645b957d9db09d7a81138f256c4c
Haixiang <haixiang6123@gmail.com>
2020-06-25 13:37:21 +0800

    读取一个GBK编码的文件

--- src/main/java/com/github/hcsp/string/GbkFileReader.java
+++ src/main/java/com/github/hcsp/string/GbkFileReader.java
@@ -4,7 +4,6 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.charset.Charset;
 import java.nio.file.Files;
-import java.util.stream.Collectors;
 
 public class GbkFileReader {
     public static void main(String[] args) throws IOException {
