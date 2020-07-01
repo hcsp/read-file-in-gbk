cb7c364d0b840dd67248ffdf4fc001194d9982a2
luhao <linesahao@outlook.com>
2020-07-01 09:57:37 +0800

    读取Bytes然后按照相应编码解码。

--- src/main/java/com/github/hcsp/string/GbkFileReader.java
+++ src/main/java/com/github/hcsp/string/GbkFileReader.java
@@ -1,10 +1,7 @@
 package com.github.hcsp.string;
 
 import java.io.*;
-import java.nio.ByteBuffer;
-import java.nio.CharBuffer;
-import java.nio.charset.Charset;
-import java.nio.charset.CharsetDecoder;
+import java.nio.file.Files;
 
 public class GbkFileReader {
     public static void main(String[] args) throws IOException {
@@ -13,16 +10,7 @@ public class GbkFileReader {
     }
 
     public String readFileWithGBK(File file) throws IOException {
-        FileInputStream inputStream = new FileInputStream(file);
-        Charset gbk = Charset.forName("GBK");
-        CharsetDecoder decoder = gbk.newDecoder();
-        StringBuilder builder = new StringBuilder();
-        byte[] bytes = new byte[64];
-        int len;
-        while ((len = inputStream.read(bytes)) != -1) {
-            CharBuffer decode = decoder.decode(ByteBuffer.wrap(bytes, 0, len));
-            builder.append(decode.toString());
-        }
-        return builder.toString();
+        byte[] bytes = Files.readAllBytes(file.toPath());
+        return new String(bytes, "GBK");
     }
 }
