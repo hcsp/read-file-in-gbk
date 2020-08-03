f549a4e167876709eefc112714268a4a870d457f
ShanQing Huang <58178504+ShanqingHuang98@users.noreply.github.com>
2020-08-03 17:39:40 +0800

    并没能力用一行代码解决这个问题 (#88)
    并没能力用一行代码解决这个问题 (#88)

--- src/main/java/com/github/hcsp/string/GbkFileReader.java
+++ src/main/java/com/github/hcsp/string/GbkFileReader.java
@@ -1,14 +1,31 @@
 package com.github.hcsp.string;
 
+
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.Charset;
+
 
 public class GbkFileReader {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         File projectDir = new File(System.getProperty("basedir", System.getProperty("user.dir")));
         System.out.println(new GbkFileReader().readFileWithGBK(new File(projectDir, "gbk.txt")));
     }
 
-    public String readFileWithGBK(File file) {
-        return null;
+    public String readFileWithGBK(File file) throws IOException {
+
+        BufferedReader strOfFile = new BufferedReader(new InputStreamReader(new FileInputStream(file.getPath()), Charset.forName("GBK")));
+        StringBuilder stringBuilder = new StringBuilder();
+        String str;
+        while ((str = strOfFile.readLine()) != null) {
+            stringBuilder.append(str);
+            stringBuilder.append("\n");
+        }
+        stringBuilder.deleteCharAt(stringBuilder.length() - 1);
+        return stringBuilder.toString();
+
     }
 }
