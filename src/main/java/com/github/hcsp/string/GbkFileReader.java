aaf4796da92161eb7c1d445ef9e4eb01d37d375e
陈晓拉尼 <2524729719@qq.com>
2020-08-04 11:00:06 +0800

    读取gbk文件 (#89)
    读取gbk文件 (#89)

--- src/main/java/com/github/hcsp/string/GbkFileReader.java
+++ src/main/java/com/github/hcsp/string/GbkFileReader.java
@@ -1,14 +1,26 @@
 package com.github.hcsp.string;
 
-import java.io.File;
+import java.io.*;
 
 public class GbkFileReader {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         File projectDir = new File(System.getProperty("basedir", System.getProperty("user.dir")));
         System.out.println(new GbkFileReader().readFileWithGBK(new File(projectDir, "gbk.txt")));
     }
 
-    public String readFileWithGBK(File file) {
-        return null;
+    public String readFileWithGBK(File file) throws IOException {
+        StringBuilder content = new StringBuilder("");
+
+        InputStream is = new FileInputStream(file);
+
+        InputStreamReader isr = new InputStreamReader(is, "gbk");
+
+        BufferedReader br = new BufferedReader(isr);
+
+        String str = "";
+        while (null != (str = br.readLine())) {
+            content.append(str + "\n");
+        }
+        return content.delete(content.length() - 1, content.length()).toString();
     }
 }
