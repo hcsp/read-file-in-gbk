4b937a13016ba038eb2ccb121cc61c389c91fafd
DobbyKim <1175088275@qq.com>
2020-08-23 17:59:29 +0800

    gbk FileReader

--- src/main/java/com/github/hcsp/string/GbkFileReader.java
+++ src/main/java/com/github/hcsp/string/GbkFileReader.java
@@ -1,14 +1,24 @@
 package com.github.hcsp.string;
 
-import java.io.File;
+import java.io.*;
 
 public class GbkFileReader {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         File projectDir = new File(System.getProperty("basedir", System.getProperty("user.dir")));
         System.out.println(new GbkFileReader().readFileWithGBK(new File(projectDir, "gbk.txt")));
     }
 
-    public String readFileWithGBK(File file) {
-        return null;
+    public String readFileWithGBK(File file) throws IOException {
+        InputStream is = new FileInputStream(file);
+        // InputStream -> String
+        BufferedReader reader = new BufferedReader(new InputStreamReader(is, "GBK"));
+        StringBuffer buffer = new StringBuffer();
+        String line = "";
+        while ((line = reader.readLine()) != null) {
+            buffer.append(line);
+            buffer.append("\n");
+        }
+        buffer.deleteCharAt(buffer.lastIndexOf("\n"));
+        return buffer.toString();
     }
 }
