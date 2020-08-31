1bd63c2ebb42769b2a1fa15861ec671c61f6f373
DING Weibing <dingweibing@outlook.jp>
2020-08-31 19:45:37 +0900

    改进仓库代码

--- src/test/java/com/github/hcsp/string/GbkFileReaderTest.java
+++ src/test/java/com/github/hcsp/string/GbkFileReaderTest.java
@@ -2,8 +2,8 @@ package com.github.hcsp.string;
 
 import java.io.File;
 import java.nio.file.Files;
-import java.util.Arrays;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -13,7 +13,7 @@ public class GbkFileReaderTest {
     public void test() throws Exception {
         File tmp = File.createTempFile("tmp", "");
 
-        String text = Arrays.stream(new String[]{"窗前明月光", "疑似地上霜", "举头望明月", "低头思故乡"})
+        String text = Stream.of("窗前明月光", "疑似地上霜", "举头望明月", "低头思故乡")
                 .collect(Collectors.joining(System.lineSeparator()));
         Files.write(tmp.toPath(), text.getBytes("GBK"));
 
