43cdf5f5636f561266f127336e7e56329a80765c
DING Weibing <dingweibing@outlook.jp>
2020-09-01 21:34:59 +0900

    my commit

--- src/main/java/com/github/hcsp/string/GbkFileReader.java
+++ src/main/java/com/github/hcsp/string/GbkFileReader.java
@@ -1,14 +1,28 @@
 package com.github.hcsp.string;
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.util.stream.Collectors;
 
 public class GbkFileReader {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         File projectDir = new File(System.getProperty("basedir", System.getProperty("user.dir")));
         System.out.println(new GbkFileReader().readFileWithGBK(new File(projectDir, "gbk.txt")));
     }
 
-    public String readFileWithGBK(File file) {
-        return null;
+    /***
+     *
+     * @param file  The path to the file
+     * @return The contents of the file as a {@code String}
+     * @throws IOException  If an I/O error occurs opening the file
+     * @throws NullPointerException If the parameter {@code file} is {@code null}
+     */
+    public String readFileWithGBK(File file) throws IOException, NullPointerException {
+        if (file == null) {
+            throw new NullPointerException();
+        }
+        return Files.lines(file.toPath(), Charset.forName("GBK")).collect(Collectors.joining(System.lineSeparator()));
     }
 }
