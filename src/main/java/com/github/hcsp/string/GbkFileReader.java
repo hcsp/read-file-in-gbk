ed0a322417198c74d1a7704595a235851576c443
summer <summer_xue.zz@foxmail.com>
2020-09-18 13:36:39 +0800

    方法二 采用list存放数据，然后用Stream加换行符

--- src/main/java/com/github/hcsp/string/GbkFileReader.java
+++ src/main/java/com/github/hcsp/string/GbkFileReader.java
@@ -5,6 +5,9 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.InputStreamReader;
 import java.nio.charset.Charset;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class GbkFileReader {
     public static void main(String[] args) {
@@ -14,16 +17,15 @@ public class GbkFileReader {
 
     public String readFileWithGBK(File file) {
         String line;
-        StringBuilder sb = new StringBuilder();
+        List<String> list = new ArrayList<>();
         try {
             BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(file), Charset.forName("GBK")));
             while ((line = br.readLine()) != null) {
-                sb.append(line).append("\n");
+                list.add(line);
             }
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
-        sb.deleteCharAt(sb.length() - 1);
-        return sb.toString();
+        return list.stream().collect(Collectors.joining(System.lineSeparator()));
     }
 }
