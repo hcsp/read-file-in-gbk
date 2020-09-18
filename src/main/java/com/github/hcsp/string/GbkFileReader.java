0909b767751d9cba7433720f895c613f6e1c0699
xuepan-summer <54898607+xuepan-summer@users.noreply.github.com>
2020-09-18 17:43:54 +0800

    Xp branch (#98)
    Xp branch (#98)

--- src/main/java/com/github/hcsp/string/GbkFileReader.java
+++ src/main/java/com/github/hcsp/string/GbkFileReader.java
@@ -1,14 +1,28 @@
 package com.github.hcsp.string;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
 
 public class GbkFileReader {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         File projectDir = new File(System.getProperty("basedir", System.getProperty("user.dir")));
         System.out.println(new GbkFileReader().readFileWithGBK(new File(projectDir, "gbk.txt")));
     }
 
-    public String readFileWithGBK(File file) {
-        return null;
+    public String readFileWithGBK(File file) throws IOException {
+        StringBuilder sb = new StringBuilder();
+        BufferedReader bf = new BufferedReader(new InputStreamReader(new FileInputStream(file), "GBK"));
+        String line = bf.readLine();
+        while (line != null) {
+            sb.append(line);
+            line = bf.readLine();
+            if (line != null) {
+                sb.append(System.lineSeparator());
+            }
+        }
+        return sb.toString();
     }
 }
