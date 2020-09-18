a9cd16429a15210b421f6c7410308bf3e8ec5892
xuepan-summer <54898607+xuepan-summer@users.noreply.github.com>
2020-09-18 13:41:08 +0800

    Xp branch (#97)
    Xp branch (#97)

--- src/main/java/com/github/hcsp/string/GbkFileReader.java
+++ src/main/java/com/github/hcsp/string/GbkFileReader.java
@@ -1,6 +1,13 @@
 package com.github.hcsp.string;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.Charset;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class GbkFileReader {
     public static void main(String[] args) {
@@ -9,6 +16,16 @@ public class GbkFileReader {
     }
 
     public String readFileWithGBK(File file) {
-        return null;
+        String line;
+        List<String> list = new ArrayList<>();
+        try {
+            BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(file), Charset.forName("GBK")));
+            while ((line = br.readLine()) != null) {
+                list.add(line);
+            }
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        return list.stream().collect(Collectors.joining(System.lineSeparator()));
     }
 }
