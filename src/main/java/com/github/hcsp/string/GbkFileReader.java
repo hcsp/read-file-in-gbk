9c768301d89c1ce78b1603a2decd2e6f23ef00ba
xuepan-summer <54898607+xuepan-summer@users.noreply.github.com>
2020-09-18 09:53:11 +0800

    GBK文件读取 (#96)
    GBK文件读取 (#96)

--- src/main/java/com/github/hcsp/string/GbkFileReader.java
+++ src/main/java/com/github/hcsp/string/GbkFileReader.java
@@ -1,6 +1,10 @@
 package com.github.hcsp.string;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.Charset;
 
 public class GbkFileReader {
     public static void main(String[] args) {
@@ -9,6 +13,17 @@ public class GbkFileReader {
     }
 
     public String readFileWithGBK(File file) {
-        return null;
+        String line;
+        StringBuilder sb = new StringBuilder();
+        try {
+            BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(file), Charset.forName("GBK")));
+            while ((line = br.readLine()) != null) {
+                sb.append(line).append("\n");
+            }
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        sb.deleteCharAt(sb.length() - 1);
+        return sb.toString();
     }
 }
