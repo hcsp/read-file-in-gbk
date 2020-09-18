7adea7883440189eb7b561c2c838523923873db2
summer <summer_xue.zz@foxmail.com>
2020-09-18 15:55:21 +0800

    方法三 修改循环条件，当下一行有数据才添加换行

--- src/main/java/com/github/hcsp/string/GbkFileReader.java
+++ src/main/java/com/github/hcsp/string/GbkFileReader.java
@@ -1,31 +1,28 @@
 package com.github.hcsp.string;
 
-import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.IOException;
 import java.io.InputStreamReader;
-import java.nio.charset.Charset;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.stream.Collectors;
+import java.io.BufferedReader;
 
 public class GbkFileReader {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         File projectDir = new File(System.getProperty("basedir", System.getProperty("user.dir")));
         System.out.println(new GbkFileReader().readFileWithGBK(new File(projectDir, "gbk.txt")));
     }
 
-    public String readFileWithGBK(File file) {
-        String line;
-        List<String> list = new ArrayList<>();
-        try {
-            BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(file), Charset.forName("GBK")));
-            while ((line = br.readLine()) != null) {
-                list.add(line);
+    public String readFileWithGBK(File file) throws IOException {
+        StringBuilder sb = new StringBuilder();
+        BufferedReader bf = new BufferedReader(new InputStreamReader(new FileInputStream(file), "GBK"));
+        String line = bf.readLine();
+        while (line != null) {
+            sb.append(line);
+            line = bf.readLine();
+            if (line != null) {
+                sb.append(System.lineSeparator());
             }
-        } catch (Exception e) {
-            throw new RuntimeException(e);
         }
-        return list.stream().collect(Collectors.joining(System.lineSeparator()));
+        return sb.toString();
     }
 }
