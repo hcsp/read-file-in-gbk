95f56aea998ef7e75ae2c3c0b9982ba40b4b57b4
qiaomengnan16 <qiaomengnan2020@outlook.com>
2020-09-25 21:10:21 +0800

    read-file-in-gbk (#100)
    read-file-in-gbk (#100)

--- src/main/java/com/github/hcsp/string/GbkFileReader.java
+++ src/main/java/com/github/hcsp/string/GbkFileReader.java
@@ -1,6 +1,9 @@
 package com.github.hcsp.string;
 
-import java.io.File;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class GbkFileReader {
     public static void main(String[] args) {
@@ -9,6 +12,18 @@ public class GbkFileReader {
     }
 
     public String readFileWithGBK(File file) {
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(file), "GBK"))) {
+            List<String> result = new ArrayList<>();
+            String line;
+            while ((line = reader.readLine()) != null) {
+                result.add(line);
+            }
+            return result.stream().collect(Collectors.joining(System.lineSeparator()));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         return null;
     }
 }
