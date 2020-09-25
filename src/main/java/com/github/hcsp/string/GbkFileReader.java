26c7eea005dda37a8c72ee1180303dab899602d1
qiaomengnan <2568151682@qq.com>
2020-09-25 21:11:49 +0800

    read-file-in-gbk

--- src/main/java/com/github/hcsp/string/GbkFileReader.java
+++ src/main/java/com/github/hcsp/string/GbkFileReader.java
@@ -1,6 +1,11 @@
 package com.github.hcsp.string;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.InputStreamReader;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.FileInputStream;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
