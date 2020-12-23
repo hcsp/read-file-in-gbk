922f832d1c410fb64031fb3ae165e6fc905e1498
hank <619805205@qq.com>
2020-12-23 21:27:59 +0800

    gbk读取文件

--- src/main/java/com/github/hcsp/string/GbkFileReader.java
+++ src/main/java/com/github/hcsp/string/GbkFileReader.java
@@ -1,14 +1,18 @@
 package com.github.hcsp.string;
 
+import org.apache.commons.io.FileUtils;
+
 import java.io.File;
+import java.io.IOException;
+
 
 public class GbkFileReader {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         File projectDir = new File(System.getProperty("basedir", System.getProperty("user.dir")));
         System.out.println(new GbkFileReader().readFileWithGBK(new File(projectDir, "gbk.txt")));
     }
 
-    public String readFileWithGBK(File file) {
-        return null;
+    public String readFileWithGBK(File file) throws IOException {
+        return FileUtils.readFileToString(file, "gbk");
     }
 }
