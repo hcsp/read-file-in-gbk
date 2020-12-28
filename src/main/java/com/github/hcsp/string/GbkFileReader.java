6eae86198eaa6f15780e03cb5584d79e9bcba35c
yang song <1479867360@qq.com>
2020-12-28 19:28:11 +0800

    读取一个GBK编码的文件 (#109)
    读取一个GBK编码的文件 (#109)

--- src/main/java/com/github/hcsp/string/GbkFileReader.java
+++ src/main/java/com/github/hcsp/string/GbkFileReader.java
@@ -1,6 +1,14 @@
 package com.github.hcsp.string;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.nio.charset.Charset;
+import java.util.ArrayList;
+import java.util.stream.Collectors;
 
 public class GbkFileReader {
     public static void main(String[] args) {
@@ -9,6 +17,18 @@ public class GbkFileReader {
     }
 
     public String readFileWithGBK(File file) {
-        return null;
+        ArrayList<String> list = new ArrayList<>();
+        try {
+            Reader reader = new InputStreamReader(new FileInputStream(file), Charset.forName("GBK"));
+            BufferedReader bufferedReader = new BufferedReader(reader);
+
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                list.add(line);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return list.stream().collect(Collectors.joining(System.lineSeparator()));
     }
 }
