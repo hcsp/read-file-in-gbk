1a64b63189856a0ff97fcd8e1b34248133cdd7fe
qipengWei <webqipeng@163.com>
2021-01-04 23:08:44 +0800

    读取流转换为GBK编码

--- src/main/java/com/github/hcsp/string/GbkFileReader.java
+++ src/main/java/com/github/hcsp/string/GbkFileReader.java
@@ -1,6 +1,10 @@
 package com.github.hcsp.string;
 
-import java.io.File;
+import java.io.*;
+import java.nio.charset.Charset;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class GbkFileReader {
     public static void main(String[] args) {
@@ -9,6 +13,26 @@ public class GbkFileReader {
     }
 
     public String readFileWithGBK(File file) {
-        return null;
+        List<String> list = new ArrayList<>();
+
+        try {
+            //读取格式为GBK编码
+            Reader reader = new InputStreamReader(new FileInputStream(file), Charset.forName("GBK"));
+            //转换为缓冲流 缓冲暂存区 一次性读取所有字节
+            BufferedReader br = new BufferedReader(reader);
+
+            String line;
+
+            while ((line = br.readLine()) != null) {
+                list.add(line);
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        //读取列表转换为字符串
+        return list.stream().collect(Collectors.joining(System.lineSeparator()));
     }
 }
