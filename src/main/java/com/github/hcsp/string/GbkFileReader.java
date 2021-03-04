8286d8cb3608f26eb2e6269463ee6dcdd3a6fe66
pccmast <248981941@qq.com>
2021-03-04 22:25:15 +0800

    实现GbkFileReader
    考虑不同操作系统换行符不同的问题

--- src/main/java/com/github/hcsp/string/GbkFileReader.java
+++ src/main/java/com/github/hcsp/string/GbkFileReader.java
@@ -15,11 +15,11 @@ public class GbkFileReader {
     public String readFileWithGBK(File file) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(file), "GBK"));
         StringBuilder builder = new StringBuilder();
-        String line;
-        while ((line = reader.readLine()) != null) {
-            builder.append(line).append("\r\n");
+        int ch;
+        while ((ch = reader.read()) != -1) {
+            builder.append((char) ch);
         }
-        String result = builder.toString();
-        return result.length() > 0 ? result.substring(0, result.length() - 2) : result;
+        return builder.toString();
+//        return result.length() > 0 ? result.substring(0, result.length() - 2) : result;
     }
 }
