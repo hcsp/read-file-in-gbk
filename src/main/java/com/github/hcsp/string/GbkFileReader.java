55c5c61c057208ef0fb2e4151352c0eab2250a76
pccmast <35651458+pccmast@users.noreply.github.com>
2021-03-04 22:26:15 +0800

    实现GbkFileReader (#113)
    实现GbkFileReader (#113)

--- src/main/java/com/github/hcsp/string/GbkFileReader.java
+++ src/main/java/com/github/hcsp/string/GbkFileReader.java
@@ -1,14 +1,25 @@
 package com.github.hcsp.string;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class GbkFileReader {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         File projectDir = new File(System.getProperty("basedir", System.getProperty("user.dir")));
         System.out.println(new GbkFileReader().readFileWithGBK(new File(projectDir, "gbk.txt")));
     }
 
-    public String readFileWithGBK(File file) {
-        return null;
+    public String readFileWithGBK(File file) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(file), "GBK"));
+        StringBuilder builder = new StringBuilder();
+        int ch;
+        while ((ch = reader.read()) != -1) {
+            builder.append((char) ch);
+        }
+        return builder.toString();
+//        return result.length() > 0 ? result.substring(0, result.length() - 2) : result;
     }
 }
