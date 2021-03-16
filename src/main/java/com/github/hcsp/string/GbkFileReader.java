89470ce3174e107bf0981b29e9b17e58338c30e1
George <jobsteng@163.com>
2021-03-16 22:52:34 +0800

    读取一个GBK编码的文件 (#114)
    读取一个GBK编码的文件 (#114)

--- src/main/java/com/github/hcsp/string/GbkFileReader.java
+++ src/main/java/com/github/hcsp/string/GbkFileReader.java
@@ -1,14 +1,20 @@
 package com.github.hcsp.string;
 
-import java.io.File;
+import java.io.*;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class GbkFileReader {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         File projectDir = new File(System.getProperty("basedir", System.getProperty("user.dir")));
         System.out.println(new GbkFileReader().readFileWithGBK(new File(projectDir, "gbk.txt")));
     }
 
-    public String readFileWithGBK(File file) {
-        return null;
+    public String readFileWithGBK(File file) throws IOException {
+        Path path = Paths.get(file.getPath());
+        byte[] bytes = Files.readAllBytes(path);
+        return new String(bytes, Charset.forName("GBK"));
     }
 }
