1deacb47962db107b3522dd721bb0df2ee9af5cd
giantpetter <65336996+giantpetter@users.noreply.github.com>
2021-06-08 22:24:48 +0800

    使用bufferread类从流中读取

--- src/main/java/com/github/hcsp/string/GbkFileReader.java
+++ src/main/java/com/github/hcsp/string/GbkFileReader.java
@@ -1,10 +1,11 @@
 package com.github.hcsp.string;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.nio.charset.Charset;
-import java.nio.file.Files;
-import java.util.List;
 
 public class GbkFileReader {
     public static void main(String[] args) {
@@ -13,12 +14,17 @@ public class GbkFileReader {
     }
 
     public String readFileWithGBK(File file) {
-        List<String> collect;
-        try {
-            collect = Files.readAllLines(file.toPath(), Charset.forName("GBK"));
+        StringBuilder str = new StringBuilder();
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(file), Charset.forName("GBK")))) {
+
+            String tmp;
+            while ((tmp = reader.readLine()) != null) {
+                str.append(tmp).append("\n");
+            }
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
-        return String.join("\n", collect);
+        str.deleteCharAt(str.length() - 1);
+        return str.toString();
     }
 }
