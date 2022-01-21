7a77c3c16b7a90da41d1ded4780df58e79b88f36
hedongsheng <he77505357@163.com>
2022-01-21 16:07:01 +0800

    字符串格式转换 (#133)
    字符串格式转换 (#133)

--- src/main/java/com/github/hcsp/string/GbkFileReader.java
+++ src/main/java/com/github/hcsp/string/GbkFileReader.java
@@ -1,14 +1,21 @@
 package com.github.hcsp.string;
 
+
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+
 
 public class GbkFileReader {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         File projectDir = new File(System.getProperty("basedir", System.getProperty("user.dir")));
         System.out.println(new GbkFileReader().readFileWithGBK(new File(projectDir, "gbk.txt")));
     }
 
-    public String readFileWithGBK(File file) {
-        return null;
+    public String readFileWithGBK(File file) throws IOException {
+//        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(new FileInputStream(file), "GBK"));
+//        return bufferedReader.lines().collect(Collectors.joining("\n"));
+        byte[] bytes = Files.readAllBytes(file.toPath());
+        return new String(bytes, "GBK");
     }
 }
