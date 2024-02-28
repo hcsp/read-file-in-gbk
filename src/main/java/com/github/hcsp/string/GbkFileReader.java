10bab9e8fa8db462e6c46d6c75efa2a1daa5f69c
rf4github <39894049+rf818@users.noreply.github.com>
2024-02-28 17:35:20 +0800

    read-file-in-gbk (#141)
    read-file-in-gbk (#141)

--- src/main/java/com/github/hcsp/string/GbkFileReader.java
+++ src/main/java/com/github/hcsp/string/GbkFileReader.java
@@ -1,6 +1,11 @@
 package com.github.hcsp.string;
 
 import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.ByteBuffer;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
 
 public class GbkFileReader {
     public static void main(String[] args) {
@@ -9,6 +14,13 @@ public class GbkFileReader {
     }
 
     public String readFileWithGBK(File file) {
-        return null;
+        try (InputStream inputStream = Files.newInputStream(file.toPath())) {
+            byte[] bytes = new byte[inputStream.available()];
+            inputStream.read(bytes);
+            Charset charset = Charset.forName("GBK");
+            return charset.decode(ByteBuffer.wrap(bytes)).toString();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
