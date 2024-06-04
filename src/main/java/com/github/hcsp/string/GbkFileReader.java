3ab2c242f3de6cb2513a2ca5a407aad6d2337af5
Leo Cheung <leozhang621@gmail.com>
2024-06-04 18:30:48 +0800

    read file in gbk

--- src/main/java/com/github/hcsp/string/GbkFileReader.java
+++ src/main/java/com/github/hcsp/string/GbkFileReader.java
@@ -3,6 +3,7 @@ package com.github.hcsp.string;
 import java.io.*;
 import java.nio.charset.Charset;
 import java.nio.file.Files;
+import java.util.StringJoiner;
 
 public class GbkFileReader {
     public static void main(String[] args) {
@@ -12,15 +13,15 @@ public class GbkFileReader {
 
     public String readFileWithGBK(File file) {
         try (BufferedReader br = new BufferedReader(new InputStreamReader(Files.newInputStream(file.toPath()), Charset.forName("GBK")))) {
-            StringBuilder sb = new StringBuilder();
+            StringJoiner sj = new StringJoiner("\n");
             while (true) {
                 String line = br.readLine();
                 if (line == null) {
                     break;
                 }
-                sb.append(line);
+                sj.add(line);
             }
-            return sb.toString();
+            return sj.toString();
         } catch (Exception e) {
             e.printStackTrace();
             return null;
